32e527159f92e4ad97802221ccbf9a7b0d0cd4e8
Deyborah <deyborah.roger@gmail.com>
2023-12-11 15:10:32 +0100

    exercice 1 terminer

---
+++ src/main/java/tp1/exercice1/moyenne.java
@@ -0,0 +1,26 @@
+package tp1.exercice1;
+import java.util.Scanner;
+
+public class moyenne {
+        public static void main(String[] args) {
+
+            /** Définir le scanner */
+            Scanner sc= new Scanner(System.in);
+
+            /** Récupérer la saisie de note 1 */
+            System.out.println("Veuillez saisir une note");
+            int note1 = scanner.nextInt();
+
+            /** Récupérer la saisie de note 2 */
+            System.out.println("Veuillez saisir une deuxième note");
+            int note2 = scanner.nextInt();
+
+            /** Récupérer la saisie de note 3 */
+            System.out.println("Veuillez saisir une troisième note");
+            int note3 = scanner.nextInt();
+
+            Moyenne= (note1+note2+note3)/3;
+
+
+        }
+}
